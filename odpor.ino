fb9f3cae61d0154c744be279126834d3564d3d5e
markomichal <48068123+markomichal@users.noreply.github.com>
2019-02-28 14:46:19 +0100

    Add files via upload

---
+++ odpor.ino
@@ -0,0 +1,17 @@
+int resist=A1;
+float port, Vm, Rm;
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin (9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+port=analogRead(resist);
+
+Vm=(port*5/1023);
+Rm=(10*(5-Vm)/Vm);
+Serial.println(Rm);
+
+}
